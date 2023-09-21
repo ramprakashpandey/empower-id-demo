9ef7a13be48580f8566e5add57e4e554d30609c9
ramprakashpandey <ramprakas.pandey786@gmail.com>
2023-09-21 13:37:39 +0530

    added-project

---
+++ SearchManagement/Models/PostDto.cs
@@ -0,0 +1,15 @@
+﻿#pragma warning disable
+
+namespace SearchManagement.Models
+{
+    /// <summary>
+    /// DTO: This data transfer object returns post as response 
+    /// </summary>
+    public class PostDto
+    {
+        public string Title { get; set; }
+        public string? Content { get; set; }
+        public DateTime PostCreated { get; set; }
+        public DateTime? PostUpdated { get; set; }
+    }
+}
